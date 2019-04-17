973aed1e01f18b48dcbf7b07d4a21a3a56da80b0
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-04-17 18:02:41 -0400

    4/17 Initial Upload

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+var friends = require('../data/friends.js');
+
+module.exports = function (app) {
+
+    app.get('/api/friends', function(req, res) {
+        res.json(friends);
+    });
+
+    app.post('/api/friends', function (req, res) {
+        if(friends.length < 5) {
+            friends.push(req.body);
+            res.json(true);
+        }else {
+            console.log("No more friends!")
+            res.json(false);
+        }
+    });
+
+    app.post('api/clear', function(){
+        friends = [];
+        console.log(friends);
+    });
+}
